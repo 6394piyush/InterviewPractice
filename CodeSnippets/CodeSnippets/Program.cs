c026ea6e983dc774f353690129ea2eca32bbeb0d
6394piyush <6394piyush@gmail.com>
2020-06-21 07:50:36 +0530

    Reverse of String

--- CodeSnippets/CodeSnippets/Program.cs
+++ CodeSnippets/CodeSnippets/Program.cs
@@ -4,9 +4,23 @@ namespace CodeSnippets
 {
     class Program
     {
+        static int MAX_NUM = 26;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "piiiyush";
+            int[] charCount = new int[MAX_NUM];
+
+            for(int i=0;i<str.Length;i++)
+            {
+                charCount[str[i] - 'a']++;
+            }
+            for(int i=MAX_NUM-1;i>0;i--)
+            {
+                for(int j=0;j<charCount[i];j++)
+                {
+                    Console.Write((char)('a' + i));
+                }
+            }
         }
     }
 }
